73b140978e5298fac15859dd81cce499f33bb493
Andrei <andrei.dima@usm.ro>
2025-02-22 14:34:28 +0100

    a

--- app/Http/Controllers/RegistruController.php
+++ app/Http/Controllers/RegistruController.php
@@ -95,7 +95,7 @@ class RegistruController extends Controller
     public function pdfExportRegistre(Request $request)
     {
         $registre = Registru::
-            take(300)
+            take(200)
             ->get();
 
         if ($request->tip === "registrul-cadastral-al-imobilelor") {
