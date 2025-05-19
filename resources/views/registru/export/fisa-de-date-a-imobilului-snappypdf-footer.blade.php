b74a2ff87bdc73b5562dd0731319859f6cad4bf4
Andrei <andrei.dima@usm.ro>
2025-05-19 20:04:50 +0200

    Tried other pdf generating packages

--- resources/views/registru/export/fisa-de-date-a-imobilului-snappypdf-footer.blade.php
+++ resources/views/registru/export/fisa-de-date-a-imobilului-snappypdf-footer.blade.php
@@ -1,7 +1,8 @@
 <div style="padding:0 10mm; width:100%; text-align:left;">
-    <p style="text-align:left; margin:0; padding:0px 20px; font-size:26px">
+    <p style="text-align:left; margin:0; padding:0px 20px; font-size:20px">
         Actul original la ID .....................................
-    </p style="padding:0; margin:0; font-size:5px;">
+    </p>
+    <div style="padding:0; margin:0; font-size:10px;">
         Nota:
         <br>
         - Pentru a prelua toate datele constatate la teren, in cazul situatiilor complexe, Prestatorul poate modifica formatul fisei de date a imobilului, cu acordul ANCPI
@@ -9,4 +10,5 @@
         - Declaratia titularului dreptului de proprietate se completeaza in cazul sectoarelor cadastrale cu deficit de suprafata.  Fisa de date este semnata in mod obligatoriu de catre titularii dreptului de proprietate.
         <br>
         - In cazul imobilelor cu posesori, fisa de date este semnata in mod obligatoriu de catre posesori
+    </div>
 </div>
