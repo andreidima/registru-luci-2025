7b352b71029d8be55eb12ba1d27691627561130f
Andrei <andrei.dima@usm.ro>
2025-05-19 20:00:43 +0200

    Tried other pdf generating packages

--- resources/views/registru/export/fisa-de-date-a-imobilului-snappypdf-footer.blade.php
+++ resources/views/registru/export/fisa-de-date-a-imobilului-snappypdf-footer.blade.php
@@ -1,7 +1,7 @@
 <div style="padding:0 10mm; width:100%; text-align:left;">
     <p style="text-align:left; margin:0; padding:0px 20px; font-size:16px">
         Actul original la ID .....................................
-    </p style="padding:0; margin:0; font-size:12.5px;">
+    </p style="padding:0; margin:0; font-size:11px;">
         Nota:
         <br>
         - Pentru a prelua toate datele constatate la teren, in cazul situatiilor complexe, Prestatorul poate modifica formatul fisei de date a imobilului, cu acordul ANCPI
